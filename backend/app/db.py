3201637316e94980a9ece2b65361641f8617a792
Loïck Balloy <loick.balloy@epita.fr>
2026-02-26 17:17:18 +0100

    Add ClientOptions to supabase client initialization

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,5 +1,5 @@
 from app.setting import settings
-from supabase import create_client, Client
+from supabase import create_client, Client, ClientOptions
 from fastapi import Depends
 
 supabase : Client = create_client(settings.database_url, settings.database_key, options=ClientOptions(
